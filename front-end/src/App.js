06642c9ffc483975d6d27efff0845e325c6826b0
brucewxh <brucewxh@dhcp-rhodes-3151.redrover.cornell.edu>
2018-11-19 16:07:25 -0500

    login updated

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo"/>
           <h1 className="App-title">Power Dialer</h1>
           <Account/>
         </header>
